ab65fc91ba8611d34417bf71963c5685b95b3467
Anders Madsen <anders.madsen@dagrofa.dk>
2025-06-10 10:27:41 +0200

    updated tests

--- tests/test_locking.py
+++ tests/test_locking.py
@@ -1,5 +1,6 @@
 # pyright: basic
 from os import path, remove, makedirs
+import sys
 from typing import cast
 from io import IOBase
 from pytest import raises as assert_raises, fixture
@@ -19,7 +20,7 @@ def test_handle():
     pos = file.tell()
     continuation_hit = []
 
-    def fn_continuation(acquired: bool, handle: IOBase, filename: str):
+    def fn_continuation(acquired: bool, handle: Handle, fp: IOBase):
         continuation_hit.append(int(acquired))
 
     try:
@@ -30,8 +31,11 @@ def test_handle():
 
         with handle1:
             assert file.tell() == pos
-            with assert_raises(LockException):
-                handle2.acquire()
+
+            if sys.platform == "win32":
+                # concurent locks in same process is prohibited on windows but not linux
+                with assert_raises(LockException):
+                    handle2.acquire()
 
         assert 1 in continuation_hit
         assert 0 in continuation_hit
@@ -60,9 +64,11 @@ def test_lock_file():
         handle1 = lock_file(filename)
         handle2 = Handle(file, filename)
 
-        with handle1:
-            with assert_raises(LockException):
-                handle2.acquire()
+        if sys.platform == "win32":
+            # concurent locks in same process is prohibited on windows but not linux
+            with handle1:
+                with assert_raises(LockException):
+                    handle2.acquire()
 
         assert not file.closed # lock_file doesn't close file
     finally:
@@ -77,13 +83,6 @@ def test_lock_handle():
 
     handle = cast(Handle, lock_handle(name))
     assert path.abspath(handle.filename) == path.abspath(file_path)
-
-    # # the shared lock path might not work in github test dev,
-    # handle.file.close()
-
-    # file_path = path.join("tests", "testdata", f"{__name__}.lock")
-    # file = open(file_path, 'w')
-    # setattr(handle, "_Handle__filename", file_path)
 
     handle.acquire()
 
